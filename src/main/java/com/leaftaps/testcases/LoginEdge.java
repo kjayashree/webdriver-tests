11bac3161147c7ed96d0af4a7f0baf2fb8452de3
kjayashree <kjayashree2007@gmail.com>
2024-12-28 16:32:55 +0530

    Update LoginEdge.java

--- src/main/java/com/leaftaps/testcases/LoginEdge.java
+++ src/main/java/com/leaftaps/testcases/LoginEdge.java
@@ -32,7 +32,7 @@ public class LoginEdge {
 		// dc.setVersion("120.0");
 		dc.setPlatform(Platform.LINUX);
 
-		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://20.40.48.160:4444/wd/hub"), dc);
+		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://20.40.50.167:4444/wd/hub"), dc);
 
 		// ChromeDriver driver = new ChromeDriver();
 
